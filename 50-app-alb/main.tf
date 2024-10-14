8123e929a4d2ac562e1a773611246405575efed3
pmadhureddy <pmadhureddy1510@gmail.com>
2024-10-14 06:31:29 +0530

    Implemented Load balancing and VPN usage

---
+++ 50-app-alb/main.tf
@@ -0,0 +1,51 @@
+module "alb" {
+  source                = "terraform-aws-modules/alb/aws"
+  internal              = true
+  name                  = "${local.resource_name}-app-alb" #expense-dev-app-alb
+  vpc_id                = local.vpc_id
+  subnets               = local.private_subnet_ids
+  security_groups       = [local.app_alb_sg_id]
+  create_security_group = false
+
+  tags = merge(
+    var.common_tags,
+    var.app_alb_tags
+  )
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = module.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+   default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hello, I am from App ALB</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+  module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+
+  zone_name = var.zone_name
+
+  records = [
+  {
+      name    = "*.app-${var.environment}"
+      type    = "A"
+      alias   = {
+        name    = module.alb.dns_name
+        zone_id = module.alb.zone_id
+      }
+      allow_overwrite = true
+    }
+
+  ]
+
+}
